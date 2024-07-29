badace41f2709c95118bb308c726475c9b2c1c43
andrusha2 <herr.dron@mail.ru>
2024-07-29 23:06:21 +0300

    fixed comments

--- blog/models.py
+++ blog/models.py
@@ -35,13 +35,6 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.title}, {self.author}'
 
-    # def get_absolute_url(self):
-    #     return reverse('blog:post_detail',
-    #                    args=[self.publish.year,
-    #                          self.publish.month,
-    #                          self.publish.day,
-    #                          self.slug])
-
     class Meta:
         ordering = ['-publish']
         indexes = [
